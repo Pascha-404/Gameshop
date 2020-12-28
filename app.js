186518621b1fabb6a7ae69cba4598d49283cdfea
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2020-12-28 22:02:14 +0100

    Added Price Value and saving

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 // Filter and cart opener/closer
 const filterBox = document.querySelector('.filterBox')
 const closeFilter = document.querySelector('#closeFilter')
@@ -22,5 +21,45 @@ closeFilter.addEventListener('click', () => {
     filterBox.classList.toggle('hidden');
 })
 
-// Get items from source
+// Get data from source
 
+let shopsList = "";
+
+async function shops() {
+   const shops = await axios.get('https://www.cheapshark.com/api/1.0/stores')
+   shopsList = shops.data;
+}
+
+
+async function deals() {
+    const deals = await axios.get('https://www.cheapshark.com/api/1.0/deals');
+
+}
+
+const makeCards = (games) => {
+    for (let results of games) {
+        if (results.thumb) {
+            const card = document.createElement('li');
+            card.classList.add('card');
+
+            const savings = document.createElement('p');
+            const savingsValue = Math.floor(parseInt(results.savings))
+            savings.id = 'savings';
+            savings.textContent = `-${savingsValue}%`
+            card.append(savings)
+
+            const cardImg = document.createElement('img');
+            cardImg.classList.add('card-img-top');
+            cardImg.src = results.thumb;
+            cardImg.alt = `${results.title} - Cover`
+            card.append(cardImg)
+
+            const cardBody = document.createElement('div');
+            cardBody.classList.add('card-body');
+
+            const cardTitle = document.createElement('h5');
+            cardTitle.classList.add('card-title');
+            cardTitle.textContent = results.title;
+        }
+    }
+}

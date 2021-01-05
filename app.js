ef6cdc002a56986aedd3cd2356ae9e2f1add61e0
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-01-05 22:51:18 +0100

    changed logic of random cards

--- app.js
+++ app.js
@@ -22,37 +22,107 @@ closeFilter.addEventListener('click', () => {
     filterBox.classList.toggle('hidden');
 })
 
-// Get data from source
+// Get data from source landingpage
 
 let movieList = document.querySelector('#movieList')
 
 let movieCache = '';
 
-async function movieGet() {
-    movieCache = await axios.get(`http://api.tvmaze.com/shows?`);
-    makeCardsRandom(movieCache.data);
-}
-movieGet()
+// const makeCardsRandom = (data) => {
+//     let i = 0;
+//     for (let results of data) {
+//         i++
+//         if (i < 20) {
 
-const makeCardsRandom = (data) => {
-    let i = 0;
-    for (let results of data) {
-        i++
-        if (i < 20) {
+//             const card = document.createElement('li');
+//             card.classList.add('card');
 
+//             const savings = document.createElement('p');
+//             const savingsValue = results.runtime;
+//             savings.id = 'savings';
+//             savings.textContent = `${savingsValue} min`
+//             card.append(savings)
+
+//             const cardImg = document.createElement('img');
+//             cardImg.classList.add('card-img-top');
+//             cardImg.src = results.image.medium;
+//             cardImg.alt = `${results.name} - Cover`
+//             card.append(cardImg)
+
+//             const cardBody = document.createElement('div');
+//             cardBody.classList.add('card-body');
+
+//             const titleContainer = document.createElement('div');
+//             titleContainer.classList.add('titleContainer');
+
+//             const cardTitle = document.createElement('h5');
+//             cardTitle.classList.add('card-title');
+//             cardTitle.textContent = results.name;
+//             titleContainer.append(cardTitle);
+
+//             const rating = results.rating.average;
+//             const ratingText = document.createElement('p');
+//             ratingText.classList.add('rating');
+//             ratingText.textContent = `Rating: ${rating}`;
+
+//             titleContainer.append(ratingText);
+
+//             cardBody.append(titleContainer)
+
+//             const cardText = document.createElement('p');
+//             cardText.classList.add('card-text');
+//             const playDay = results.schedule.days;
+//             const playTime = results.schedule.time;
+//             cardText.textContent = `Runs on ${playDay} at ${playTime}`;
+//             cardBody.append(cardText)
+
+//             const btnGroup = document.createElement('div');
+//             btnGroup.classList.add('btn-group');
+
+//             const goToPage = document.createElement('a');
+//             goToPage.classList.add('btn');
+//             goToPage.href = results.url;
+//             goToPage.target = '_blank'
+//             goToPage.textContent = 'Go to Movie';
+//             btnGroup.append(goToPage);
+//             const officPage = document.createElement('a');
+//             officPage.classList.add('btn');
+//             officPage.textContent = 'Official Page';
+//             officPage.href = results.officialSite;
+//             officPage.target = '_blank'
+//             btnGroup.append(officPage)
+
+//             cardBody.append(btnGroup)
+
+//             card.append(cardBody)
+
+//             movieList.append(card)
+
+//         }
+//     }
+// }
+
+const makeCardsRandom2 = (data) => {
+    let randomNmbrCache = [];
+
+    for (let j = 0; j < 20; j++) {
+        const randomNmbr = Math.floor((Math.random() * 240) + 1);
+        randomNmbrCache.push(randomNmbr)
+
+        if (randomNmbrCache.includes(randomNmbr) === false) {
             const card = document.createElement('li');
             card.classList.add('card');
 
             const savings = document.createElement('p');
-            const savingsValue = results.runtime;
+            const savingsValue = data[randomNmbr].runtime;
             savings.id = 'savings';
             savings.textContent = `${savingsValue} min`
             card.append(savings)
 
             const cardImg = document.createElement('img');
             cardImg.classList.add('card-img-top');
-            cardImg.src = results.image.medium;
-            cardImg.alt = `${results.name} - Cover`
+            cardImg.src = data[randomNmbr].image.medium;
+            cardImg.alt = `${data[randomNmbr].name} - Cover`
             card.append(cardImg)
 
             const cardBody = document.createElement('div');
@@ -63,10 +133,10 @@ const makeCardsRandom = (data) => {
 
             const cardTitle = document.createElement('h5');
             cardTitle.classList.add('card-title');
-            cardTitle.textContent = results.name;
+            cardTitle.textContent = data[randomNmbr].name;
             titleContainer.append(cardTitle);
 
-            const rating = results.rating.average;
+            const rating = data[randomNmbr].rating.average;
             const ratingText = document.createElement('p');
             ratingText.classList.add('rating');
             ratingText.textContent = `Rating: ${rating}`;
@@ -77,8 +147,8 @@ const makeCardsRandom = (data) => {
 
             const cardText = document.createElement('p');
             cardText.classList.add('card-text');
-            const playDay = results.schedule.days;
-            const playTime = results.schedule.time;
+            const playDay = data[randomNmbr].schedule.days;
+            const playTime = data[randomNmbr].schedule.time;
             cardText.textContent = `Runs on ${playDay} at ${playTime}`;
             cardBody.append(cardText)
 
@@ -87,14 +157,14 @@ const makeCardsRandom = (data) => {
 
             const goToPage = document.createElement('a');
             goToPage.classList.add('btn');
-            goToPage.href = results.url;
+            goToPage.href = data[randomNmbr].url;
             goToPage.target = '_blank'
             goToPage.textContent = 'Go to Movie';
             btnGroup.append(goToPage);
             const officPage = document.createElement('a');
             officPage.classList.add('btn');
             officPage.textContent = 'Official Page';
-            officPage.href = results.officialSite;
+            officPage.href = data[randomNmbr].officialSite;
             officPage.target = '_blank'
             btnGroup.append(officPage)
 
@@ -105,8 +175,17 @@ const makeCardsRandom = (data) => {
             movieList.append(card)
 
         }
+        continue
     }
+
 }
+
+
+async function movieGet() {
+    movieCache = await axios.get(`http://api.tvmaze.com/shows?`);
+    makeCardsRandom2(movieCache.data);
+}
+movieGet()
 
 // Search Form
 
